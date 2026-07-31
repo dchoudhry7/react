f6f74a1f6f5f0f19e722b2f199f2e5e54c69edc3
Dhairya <dchoudhry999@gmail.com>
2026-07-31 12:47:56 +0530

    component build

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,7 +1,10 @@
+import Test from './Test';
+
 function App() {
   return (
     <>
     <h1>Vite + React</h1>
+    <Test/>
     </>
   )
 }
---
+++ vite-react/src/Test.jsx
@@ -0,0 +1,9 @@
+function Test() {
+    return (
+        <>
+        <p>Test</p>
+        </>
+    )
+}
+
+export default Test;
