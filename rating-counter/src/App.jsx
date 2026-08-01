81469eb39381220601824508cd779f29b1245f63
Dhairya <dchoudhry999@gmail.com>
2026-08-01 15:01:03 +0530

    hooks debut

---
+++ rating-counter/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(0)
+
+  let increase = () => {
+    if(counter < 10) setCounter(counter+1)
+  }
+
+  let decrease = () => {
+    if(0 < counter) setCounter(counter-1)
+  }
+
+  return (
+    <div>
+      <h1>Rating: {counter}</h1>
+      <div>
+        <button onClick={increase}>📈</button>
+        <br/>
+        <button onClick={decrease}>📉</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
